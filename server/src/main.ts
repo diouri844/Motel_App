62178fea25a0741815ce3bc9e90027a7948240e0
Chopen844 <Salah Eddine>
2025-05-02 19:39:18 +0200

    refactor: remove unused error handler and init service imports; integrate morgan middleware for logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,8 @@
 // setup express app : 
 import express from 'express';
 import bodyParser from 'body-parser';
-// import custom error handler middleware : 
-// import errorHandler from "../microservices/common/middleware/errorHandler"
-// import initRoutes from "../init/Service/init.services";
 import cors from 'cors';
+import morganMiddleware from './config/morganMiddleware';
 const app = express();
 
 function initRoutes(app: express.Application) { }
@@ -13,10 +11,9 @@ function initRoutes(app: express.Application) { }
 // setup express app :
 app.use(bodyParser.json());
 app.use(cors());
-//app.use(errorHandler);
+app.use(morganMiddleware);
 
 // import the setup init service functions : 
 initRoutes(app);
-// setup basePermissions : 
-// export the app as a default to use it in the server entry point : server.ts :
+
 export default app;
