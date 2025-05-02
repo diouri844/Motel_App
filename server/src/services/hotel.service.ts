c63aca95e96e6e8675b36218a019a87d19567941
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-02 23:17:03 +0200

    feat: implement HotelService with CRUD methods for hotel management

---
+++ server/src/services/hotel.service.ts
@@ -0,0 +1,49 @@
+import { HotelAbstract } from "../abstract/hotel.abstract";
+import { Hotel, PrismaClient } from "../generated/prisma/client";
+import { CreateHotelDto } from "../types/createHotel.dto";
+
+export class HotelService extends HotelAbstract {
+
+
+    async createHotel(
+        name: string,
+        location: string
+    ): Promise<Hotel | null> {
+        try {
+            const hotelPayload: CreateHotelDto = {
+                name,
+                location,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+            const newCreatedHotel: Hotel = await this.prismaClient.hotel.create({
+                data: hotelPayload,
+            });
+            return newCreatedHotel;
+        } catch (error) {
+            this.logger.error("Error creating hotel:", error);
+            return null;
+        }
+    }
+    async getHotelById(hotelId: string): Promise<Hotel | null> {
+
+        try {
+            const targetHotel: Hotel | null = await this.prismaClient.hotel.findUnique({
+                where: { id: hotelId },
+                include: {
+                    rooms: true,
+                },
+            });
+            if (!targetHotel) return null;
+            return targetHotel;
+        } catch (error) {
+            this.logger.error("Error fetching hotel by ID:", error);
+            return null;
+        }
+    };
+
+    async getAllHotels(filters?: object): Promise<Hotel[]> { return [] as Hotel[]; }
+    async updateHotel(hotelId: string, updates: object): Promise<Hotel | null> { return null; }
+
+    async deleteHotel(hotelId: string): Promise<boolean> { return false; }
+}
