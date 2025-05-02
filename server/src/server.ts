81cccc03f09cd036bb943f4777b5759f446b80f4
Chopen844 <Salah Eddine>
2025-05-02 22:16:29 +0200

    feat: add Prisma service connection and disconnection on server start and close

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
 import logger from "./config/logger";
 import app from "./main"
+import { prismaServiceProvider } from "./services/prisma.service";
 
 const port = process.env.PORT || 3000;
 
 app.listen(
     port,
-    () => {
+    async () => {
+        await prismaServiceProvider.connect();
         logger.info(`Server is listening on http://localhost:${port}/api/v1`);
     });
+
+app.on("close", async () => {
+    await prismaServiceProvider.disconnect();
+    logger.info("Server is shutting down...");
+});
