75448ebb692377a7eff4645b662ac59ed2bf19f2
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-03 02:52:34 +0200

    feat: update HotelService to enhance CRUD operations and add error handling

--- server/src/services/hotel.service.ts
+++ server/src/services/hotel.service.ts
@@ -1,6 +1,8 @@
-import { HotelAbstract } from "../abstract/hotel.abstract";
+import { HotelAbstract } from "../abstract/services/hotel.abstract";
 import { Hotel, PrismaClient } from "../generated/prisma/client";
 import { CreateHotelDto } from "../types/createHotel.dto";
+import { Pagination } from "../types/paginate.type";
+import { UpdateHotelDto } from "../types/updateHotel.dto";
 
 export class HotelService extends HotelAbstract {
 
@@ -43,16 +45,17 @@ export class HotelService extends HotelAbstract {
     };
 
     async getAllHotels(
-        paginate: object,
-        filters?: object
+        paginate: Pagination
     ): Promise<Hotel[]> {
         try {
+            const skip = (paginate.page - 1) * paginate.perPage;
+            const take = paginate.perPage;
             const hotelList: Hotel[] = await this.prismaClient.hotel.findMany({
-                where: filters,
                 include: {
                     rooms: true,
                 },
-                ...paginate,
+                skip,
+                take
             });
             return hotelList;
         }
@@ -63,8 +66,35 @@ export class HotelService extends HotelAbstract {
     }
     async updateHotel(
         hotelId: string,
-        updates: object
-    ): Promise<Hotel | null> { return null; }
+        newPayload: UpdateHotelDto
+    ): Promise<Hotel | null> {
+        try {
+            const updatedHotel: Hotel | null = await this.prismaClient.hotel.update({
+                where: { id: hotelId },
+                data: {
+                    ...newPayload,
+                    updatedAt: new Date(),
+                },
+            });
+            return updatedHotel;
+        }
+        catch (error) {
+            this.logger.error("Error updating hotel:", error);
+            return null;
+        }
+    }
 
-    async deleteHotel(hotelId: string): Promise<boolean> { return false; }
+    async deleteHotel(
+        hotelId: string
+    ): Promise<boolean> {
+        try {
+            await this.prismaClient.hotel.delete({
+                where: { id: hotelId },
+            });
+            return true;
+        } catch (error) {
+            this.logger.error("Error deleting hotel:", error);
+            return false;
+        }
+    }
 }
