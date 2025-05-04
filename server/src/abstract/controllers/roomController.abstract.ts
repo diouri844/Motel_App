30de1e189fb388b2b12eb7b58ddfb5eec6dfed19
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-04 19:48:52 +0200

    feat: add abstract RoomController class for room management operations

---
+++ server/src/abstract/controllers/roomController.abstract.ts
@@ -0,0 +1,46 @@
+import { RoomService } from "../../services/room.service";
+import { Request, Response } from "express";
+import { Logger as winstonLogger } from "winston";
+import logger from "../../config/logger";
+import { ApiResponse } from "../../types/apiResponse.type";
+import { Hotel, Room } from "../../generated/prisma/client";
+
+export abstract class RoomControllerAbstract {
+    protected readonly serviceProvider: RoomService = new RoomService();
+    protected readonly logger: winstonLogger = logger;
+
+    abstract createRoom(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse<Room>>;
+
+
+    abstract findRoomById(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse<Room>>;
+
+
+    abstract updateRoom(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse<Room>>;
+
+    abstract deleteRoom(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse>;
+
+
+    abstract findHotelRoomList(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse<Hotel[]>>
+
+
+    abstract roomAvailabilityCheck(
+        req: Request,
+        res: Response
+    ): Promise<ApiResponse>;
+
+}
