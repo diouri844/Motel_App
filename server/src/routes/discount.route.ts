7171b587b0bac3082fef28d3910ad22120e46ec5
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-05 02:41:44 +0200

    feat: implement DiscountRoute class for managing discount-related routes

---
+++ server/src/routes/discount.route.ts
@@ -0,0 +1,26 @@
+import { DiscountRouteAbstract } from "../abstract/routes/discountRoute.abstract"
+import { DiscountController } from "../controllers/discount.controller";
+
+export class DiscountRoute extends DiscountRouteAbstract {
+    constructor(discountController: DiscountController) {
+        super(discountController);
+    }
+
+    protected initializeRoutes(): void {
+        this.router.post(
+            this.path + "/create", this.discountController.createDiscount.bind(this.discountController));
+        this.router.post(
+            this.router + "/apply", this.discountController.applyDiscount.bind(this.discountController));
+
+        this.router.get(
+            this.path + "/item/:disountId", this.discountController.findDiscountById.bind(this.discountController));
+        this.router.get(
+            this.path + "/list", this.discountController.getAllDiscounts.bind(this.discountController));
+        this.router.get(
+            this.path + "/valide/list", this.discountController.getValidDiscounts.bind(this.discountController));
+        this.router.get(
+            this.path + "/check/:disouncId", this.discountController.checkDiscount.bind(this.discountController));
+        this.router.delete(
+            this.path + "/item/:id", this.discountController.deleteDiscount.bind(this.discountController));
+    }
+};
