260e9344b8ed163a904e369131b2588147531309
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-05 04:08:20 +0200

    feat: update ReservationControllerAbstract to return ApiResponse types for reservation methods

--- server/src/abstract/controllers/reservationController.abstract.ts
+++ server/src/abstract/controllers/reservationController.abstract.ts
@@ -2,13 +2,15 @@ import { Request, Response } from "express";
 import { Logger as winstonLogger } from "winston";
 import logger from "../../config/logger";
 import { DiscountService } from "../../services/discount.service";
+import { ApiResponse } from "../../types/apiResponse.type";
+import { Reservation } from "../../generated/prisma/client";
 
 export abstract class ReservationControllerAbstract {
     protected readonly serviceProvider: DiscountService = new DiscountService();
     protected readonly logger: winstonLogger = logger;
 
-    abstract createReservation(req: Request, res: Response): Promise<void>;
-    abstract getReservationById(req: Request, res: Response): Promise<void>;
-    abstract getAllReservations(req: Request, res: Response): Promise<void>;
-    abstract deleteReservation(req: Request, res: Response): Promise<void>;
+    abstract createReservation(req: Request, res: Response): Promise<ApiResponse<Reservation>>;
+    abstract getReservationById(req: Request, res: Response): Promise<ApiResponse<Reservation>>;
+    abstract getAllReservations(req: Request, res: Response): Promise<ApiResponse<Reservation[]>>;
+    abstract deleteReservation(req: Request, res: Response): Promise<ApiResponse>;
 }
