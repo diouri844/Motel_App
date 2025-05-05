d1d55d59fff6f9bc047953b5d9cae54777fd831c
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-05 03:51:54 +0200

    fix: correct route path for applyDiscount and checkDiscount methods

--- server/src/routes/discount.route.ts
+++ server/src/routes/discount.route.ts
@@ -10,7 +10,7 @@ export class DiscountRoute extends DiscountRouteAbstract {
         this.router.post(
             this.path + "/create", this.discountController.createDiscount.bind(this.discountController));
         this.router.post(
-            this.router + "/apply", this.discountController.applyDiscount.bind(this.discountController));
+            this.path + "/apply", this.discountController.applyDiscount.bind(this.discountController));
 
         this.router.get(
             this.path + "/item/:disountId", this.discountController.findDiscountById.bind(this.discountController));
@@ -19,7 +19,7 @@ export class DiscountRoute extends DiscountRouteAbstract {
         this.router.get(
             this.path + "/valide/list", this.discountController.getValidDiscounts.bind(this.discountController));
         this.router.get(
-            this.path + "/check/:disouncId", this.discountController.checkDiscount.bind(this.discountController));
+            this.path + "/check/:discountCode", this.discountController.checkDiscount.bind(this.discountController));
         this.router.delete(
             this.path + "/item/:id", this.discountController.deleteDiscount.bind(this.discountController));
     }
