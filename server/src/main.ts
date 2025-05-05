f92f19eeeb0ba4bceb31114a496ee4a186d18fe2
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-05 02:42:03 +0200

    feat: add DiscountRoute and DiscountController to main application routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,8 @@ import { RoomRoute } from './routes/room.route';
 import { RoomController } from './controllers/room.controller';
 import { GuestRoute } from './routes/guest.route';
 import { GuestController } from './controllers/guest.controller';
+import { DiscountController } from './controllers/discount.controller';
+import { DiscountRoute } from './routes/discount.route';
 const app = express();
 export const prefixer = "/api/v1";
 function initRoutes(app: express.Application): void {
@@ -16,11 +18,13 @@ function initRoutes(app: express.Application): void {
     const hotelRoute = new HotelRoute(new HotelController);
     const roomRoute = new RoomRoute(new RoomController);
     const guestRoute = new GuestRoute(new GuestController);
+    const discountRoute = new DiscountRoute(new DiscountController);
 
     // Use the routes
     app.use("/", hotelRoute.getRouter());
     app.use("/", roomRoute.getRouter());
     app.use("/", guestRoute.getRouter());
+    app.use("/", discountRoute.getRouter());
 }
 
 // setup express app :
