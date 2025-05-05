687aef87f97b8995d73baf1cbf4ca6685cacd1b0
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-05 21:15:03 +0200

    refactor: remove unused Redux provider for CRM store in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import routerSetup from "@/Providers/router.provider"
-import { Provider } from 'react-redux';
-import CrmStore from '@/stores/CRM/crm.store.ts'
 import { AxiosProvider } from "@/context/axios.context.tsx"
 
 import {
@@ -14,10 +12,9 @@ import {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AxiosProvider>
-      <Provider store={CrmStore}>
+      
         <RouterProvider router={routerSetup} />
         <App />
-      </Provider>
     </AxiosProvider>
   </React.StrictMode>
 )
