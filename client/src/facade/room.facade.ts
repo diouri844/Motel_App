1e0f6a00ded95e2b64445d0e036be46eb6657cca
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-05-06 02:57:42 +0200

    feat: add RoomFacade for managing room-related operations

---
+++ client/src/facade/room.facade.ts
@@ -0,0 +1,38 @@
+import BaseFacade from "@/facade/base.facade";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { axiosConfig } from "@/axios.setup";
+
+
+
+class RoomFacade implements BaseFacade {
+    path = "/rooms";
+    axiosInstance: AxiosInstance = axios.create({
+        ...axiosConfig
+    });
+
+    constructor() {
+        this.axiosInstance.interceptors.request.use(
+            config => { return config },
+            error => { return Promise.reject(error) }
+        );
+        this.axiosInstance.interceptors.response.use(
+            response => { return response },
+            error => { return Promise.reject(error) }
+        );
+    };
+
+    // get all rooms : 
+    async getRoomList(): Promise<AxiosResponse> {
+        return await this.axiosInstance.get(
+            this.path + "/list", {
+            headers: {
+                page: 1,
+                perPage: 100
+            }
+        }
+        );
+    }
+}
+
+
+export default RoomFacade;
