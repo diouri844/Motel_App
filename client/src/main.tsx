97face4d86e4b4a56caf5fe2a502190f646f3961
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-06-25 00:06:57 +0200

    fix: remove AxiosProvider wrapper from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import routerSetup from "@/Providers/router.provider"
-import { AxiosProvider } from "@/context/axios.context.tsx"
 
 import {
   RouterProvider,
@@ -11,10 +10,8 @@ import {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AxiosProvider>
       
         <RouterProvider router={routerSetup} />
         <App />
-    </AxiosProvider>
   </React.StrictMode>
 )
