46312abbc755b695ee1527f4192390285d18e78f
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-07-03 22:37:45 +0200

    feat: add GuestFacade class for managing guest operations

---
+++ client/src/facade/guest.facade.ts
@@ -0,0 +1,17 @@
+import client from "@/context/superBaseclietn.context";
+import BaseFacade from "@/facade/base.facade";
+// import { reservationType } from "@/types/reservation.type";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+
+
+
+class GuestFacase implements BaseFacade {
+    client: SupabaseClient = client;
+    // create new guest details : 
+    async createGuest(payload: any): Promise<any> {
+        return await this.client.from("guests").insert(payload);
+    }
+}
+
+export default GuestFacase;
