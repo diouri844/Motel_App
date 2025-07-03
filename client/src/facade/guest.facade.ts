567c82adb77751696d5da7737a63db3ee6f65dbd
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-07-04 00:10:02 +0200

    feat: enhance createGuest method to include unique ID generation for new guests

--- client/src/facade/guest.facade.ts
+++ client/src/facade/guest.facade.ts
@@ -2,6 +2,7 @@ import client from "@/context/superBaseclietn.context";
 import BaseFacade from "@/facade/base.facade";
 // import { reservationType } from "@/types/reservation.type";
 import { SupabaseClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -10,7 +11,9 @@ class GuestFacade implements BaseFacade {
     client: SupabaseClient = client;
     // create new guest details : 
     async createGuest(payload: any): Promise<any> {
-        return await this.client.from("guests").insert(payload);
+        return await this.client.from("guests").insert(
+            { ...payload, id: uuidv4() }
+        ).select("*").single();
     }
 }
 
